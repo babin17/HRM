e21c7ed4e488562a77aa5abcc2d30f740ee383de
unknown <np03a170232@heraldcollege.edu.np>
2022-04-11 13:59:16 +0545

    Added department model

--- src/main/java/com/example/hrmdl/Department.java
+++ src/main/java/com/example/hrmdl/Department.java
@@ -1,5 +1,35 @@
 package com.example.hrmdl;
 
 public class Department {
-    
+    private final String id;
+    private String name;
+    private int departmentNo;
+
+    private Department(String id, String name, int departmentNo){
+        this.id = id;
+        this.name = name;
+        this.departmentNo = departmentNo;
+    }
+
+
+    public String getName(){
+
+    }
+
+    public int getDepartmentNumber(){
+
+    }
+
+    public int getByID(){
+
+    }
+
+
+    public void setUsername(){
+
+    }
+
+    public void setDepartmentNo(){
+        
+    }
 }
